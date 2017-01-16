00c6a48d0aebd480a2f3f550b2931a08d3231832
Your Name <you@example.com>
2017-01-16 20:03:55 +0300

    initial

---
+++ listviewdelegate.h
@@ -0,0 +1,11 @@
+#ifndef LISTVIEWDELEGATE_H
+#define LISTVIEWDELEGATE_H
+
+
+class ListViewDelegate : public QStyledItemDelegate
+{
+public:
+    ListViewDelegate();
+};
+
+#endif // LISTVIEWDELEGATE_H
